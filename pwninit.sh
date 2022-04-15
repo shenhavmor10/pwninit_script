a07296f561b8837b6a66abf27c522776628ba241
Shenhav <shenhavmor10@gmail.com>
2022-04-15 15:12:19 +0300

    add template path and add the possibility of having alterantive usages in the sh file

--- pwninit.sh
+++ pwninit.sh
@@ -7,8 +7,30 @@ do
     esac
 done
 
+
+#!/bin/bash
+
+while getopts b:l: flag
+do
+    case "${flag}" in
+        b) binary=${OPTARG};;
+        l) libc_file=${OPTARG};;
+    esac
+done
+
 #CHANGE <pwninit> to a path to the pwninit exe
 #CHANGE <pwninit_patched> to a path to the pwninit_patched exe
 cp -t $(pwd) <pwninit> <pwninit_patched>
 
-./pwninit --bin $binary --libc $libc_file --template-path $template
+args=()
+args+=('--bin' $binary)
+if [! -z $libc] 
+then 
+    args+=('--libc' $libc)
+fi
+#remove this if you dont want a template of your own:)
+#if you do want a template path go ahead and change template_path
+#to the path of the template file
+args+=('--template-path' '<template_path>')
+
+./pwninit "${args[@]}"
